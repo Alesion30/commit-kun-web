06eef66dbe0f9a4d86f7703d046c327ea8cfd6bc
Yuta Ohira <alesion90@gmail.com>
2021-08-28 18:37:51 +0900

    install: tailwindcssを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { VFC } from "react";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
