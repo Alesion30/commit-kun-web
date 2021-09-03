fedb7c78d3113bd1ca239267f1864bddf71b1cbf
Yuta Ohira <alesion90@gmail.com>
2021-09-03 10:41:05 +0900

    add: ProgressCircleIconコンポーネントを実装

---
+++ src/components/molecules/progress_circle_icon.tsx
@@ -0,0 +1,22 @@
+import { VFC, useState, useEffect } from "react";
+import { Circle } from "react-circle";
+
+type ProgressCircleIconProps = {
+  percentage: number;
+};
+
+/**
+ * Molecule: プログレスバー付きアイコン
+ *
+ * https://github.com/zzarcon/react-circle
+ */
+export const ProgressCircleIcon: VFC<ProgressCircleIconProps> = ({
+  percentage,
+}) => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(setCount, 100, percentage);
+  }, [percentage]);
+
+  return <Circle animate={true} progress={count} />;
+};
