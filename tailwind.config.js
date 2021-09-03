33d34d810d9fde2f7d68df29561e6ef1134c4655
Yuta Ohira <alesion90@gmail.com>
2021-09-03 17:17:45 +0900

    add: purge対象のクラスを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    options: {
+      whitelist: [
+        "bg-red-400",
+        "bg-pink-400",
+        "bg-blue-400",
+        "bg-green-400",
+        "bg-yellow-400",
+        "bg-gray-400",
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
