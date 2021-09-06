4fd7e6c5659562903dce3303e7e3ae980ac997f9
talisman <s.k.recode226@gmail.com>
2021-09-06 23:41:20 +0900

    fix: 不要なコメント文を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,21 +27,6 @@ const Home: NextPage = () => {
         </SimpleCard>
       </div>
     </MainLayout>
-    // <MainLayout>
-    //   <div className="mt-5 text-center">
-    //     <button
-    //       className="px-2 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-    //       onClick={() => {
-    //         whileLoading(async () => {
-    //           // await setTimeout(() => { }, 2000);
-    //           await new Promise((resolve) => setTimeout(resolve, 2000));
-    //         });
-    //       }}
-    //     >
-    //       loadingを開始
-    //     </button>
-    //   </div>
-    // </MainLayout>
   );
 };
 
