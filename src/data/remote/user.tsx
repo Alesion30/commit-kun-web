b79758dd49683f2b244140d8ad8f6c261e97c4be
Yuta Ohira <alesion90@gmail.com>
2021-09-06 23:05:45 +0900

    add: ログイン時にAPIを呼び出し

---
+++ src/data/remote/user.tsx
@@ -0,0 +1,31 @@
+import axios from "~/plugins/axios";
+
+export type UserResponse = {
+  userUid: string;
+  userName: string;
+  email: string;
+  imageUrl: string;
+  accessToken: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+};
+
+export type CreateUserRequest = {
+  userName: string;
+  email: string;
+  imageUrl: string;
+  accessToken: string; // GitHubAPIトークン
+};
+
+/** ユーザー情報取得 */
+export const getUser = (token: string) =>
+  axios(token).get<UserResponse>("/user");
+
+/** ユーザー登録 */
+export const createUser = (req: CreateUserRequest, token: string) =>
+  axios(token).post<void>("/user", req);
+
+/** ユーザー情報更新 */
+export const updateUser = (req: CreateUserRequest, token: string) =>
+  axios(token).patch<void>("/user", req);
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,12 +1,28 @@
 import axiosBase from "axios";
 
-const axios = axiosBase.create({
-  baseURL: "http://localhost:8880",
-  headers: {
-    "Content-Type": "application/json",
-    "X-Requested-With": "XMLHttpRequest",
-  },
-  responseType: "json",
-});
+const axios = (token?: string) => {
+  const customAxios = axiosBase.create({
+    // baseURL: "http://localhost:8880",
+    baseURL: "https://commit-kun.herokuapp.com",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+      Authorization: `Bearer ${token}`,
+    },
+    responseType: "json",
+  });
+
+  customAxios.interceptors.request.use((request) => {
+    console.log("Starting Request: ", request);
+    return request;
+  });
+
+  customAxios.interceptors.response.use((response) => {
+    console.log("Response: ", response);
+    return response;
+  });
+
+  return customAxios;
+};
 
 export default axios;
