80d73de88d02bc4178137854cfa501c0e0c581f7
Yuta Ohira <alesion90@gmail.com>
2021-09-09 23:33:55 +0900

    add: GitHubのユーザー名を取得する処理を実装

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -9,6 +9,7 @@ import { createUser, getUser, updateUser } from "~/data/remote/user";
 import { withoutAuth } from "~/hocs";
 import { useAuth } from "~/hooks";
 import { signInGithub, credentialFromResultGitHub } from "~/plugins/firebase";
+import { Octokit, App } from "octokit";
 
 const Signin: NextPage = () => {
   const auth = useAuth();
@@ -23,14 +24,22 @@ const Signin: NextPage = () => {
         setGitHubToken(token);
 
         try {
+          // GitHub ユーザー情報
+          const octokit = new Octokit({ auth: token });
+          const githubUser = (await octokit.rest.users.getAuthenticated()).data;
+
+          // Firebase ユーザー情報
           const user = result.user;
           const idToken = await user.getIdToken();
+
+          // バックエンド ユーザー情報
           const backendUser = (await getUser(idToken)).data;
+
           if (!(backendUser && backendUser.userUid !== "")) {
             // ユーザー登録
             await createUser(
               {
-                userName: user.displayName,
+                userName: githubUser.login,
                 email: user.email,
                 imageUrl: user.photoURL,
                 accessToken: token,
@@ -41,7 +50,7 @@ const Signin: NextPage = () => {
             // ユーザー登録
             await updateUser(
               {
-                userName: user.displayName,
+                userName: githubUser.login,
                 email: user.email,
                 imageUrl: user.photoURL,
                 accessToken: token,
