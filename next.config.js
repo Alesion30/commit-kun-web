a28afcdeee3b3c8d5b8adaecbb10bbaab3d988fd
Yuta Ohira <alesion90@gmail.com>
2021-09-10 19:19:09 +0900

    add: sassの設定

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "assets/style")],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
