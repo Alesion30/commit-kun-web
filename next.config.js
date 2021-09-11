c04bc557c898d3bb6b7581a8bf0896fb2a6df395
Yuta Ohira <alesion90@gmail.com>
2021-09-11 12:08:40 +0900

    fix: 本番環境ではconsole.logを非表示に設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+// 本番環境
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
@@ -10,6 +14,19 @@ module.exports = {
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+
+    // 本番環境のみ、consoleを非表示
+    config.optimization.minimize = isProduction;
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: isProduction,
+          },
+        },
+        extractComments: "all",
+      }),
+    ];
 
     return config;
   },
